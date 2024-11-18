3836c6b6e477aa7a6399dfc806ddeb9d36469de4
Lucas <ls4803326@gmail.com>
2024-11-18 11:46:08 -0300

    add new screen /raffle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
-import { IconGift } from '@tabler/icons-react'
-import Pagina from './components/template/Pagina'
-import Titulo from './components/template/Titulo'
+import Image from "next/image";
+import { IconGift } from "@tabler/icons-react";
+import Pagina from "./components/template/Pagina";
+import Titulo from "./components/template/Titulo";
 
 export default function Home() {
-    return (
+  return (
     <Pagina className="flex flex-col gap-10">
-        <Titulo icone={IconGift} principal="Início" segundario="Festa SL Alimentos 2024" />
+      <Titulo
+        icone={IconGift}
+        principal="Início"
+        segundario="Festa SL Alimentos 2024"
+      />
     </Pagina>
-    )
+  );
 }
